9e1c511347a937a732d304930110bcd545c5ebb2
placcbo <168788887+placcbo@users.noreply.github.com>
2025-04-01 10:02:06 +0000

    ...

--- src/Container/Header/Header.jsx
+++ src/Container/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import {AppWrap} from '../../wrapper'
 
 
 import {Images} from '../../Constants';
@@ -68,4 +69,4 @@ const Header = () => (
   </div>
 );
 
-export default Header
+export default AppWrap(Header,"home")
