c4e54205c2a9924ec7a12217d5323f54c422fa46
placcbo <168788887+placcbo@users.noreply.github.com>
2025-04-19 11:46:07 +0000

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import {About,Footer,Header,Skills,Work} from './Container'
 import { Navbar } from './Components'
 import './App.scss'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 
 const App = () => {
   return (
     <div className="app" id = "home">
+      <SpeedInsights/>
         <Navbar/>
            <Header/>
            <About/>
